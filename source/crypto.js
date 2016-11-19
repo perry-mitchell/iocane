6b42870948d9cbcd162768a4fe06f2a409b41731
Perry Mitchell <perry@perrymitchell.net>
2016-11-19 16:49:26 +0200

    Add debug - closes #9

--- source/crypto.js
+++ source/crypto.js
@@ -1,95 +1,98 @@
-(function(module) {
+"use strict";
 
-    "use strict";
+var Crypto = require("crypto");
 
-    var Crypto = require("crypto");
+var derivation = require("./derive.js"),
+    generation = require("./generators.js"),
+    packing = require("./packers.js"),
+    security = require("./security.js"),
+    config = require("./config.js"),
+    debug = require("./debug.js");
 
-    var derivation = require("./derive.js"),
-        generation = require("./generators.js"),
-        packing = require("./packers.js"),
-        security = require("./security.js"),
-        config = require("./config.js");
+var lib = module.exports = {
 
-    var lib = module.exports = {
+    encrypt: function(text, keyDerivationInfo) {
+        debug("encrypt content");
+        var iv = generation.generateIV(),
+            ivHex = iv.toString("hex");
+        var encryptTool = Crypto.createCipheriv(config.ENC_ALGORITHM, keyDerivationInfo.key, iv),
+            hmacTool = Crypto.createHmac(config.HMAC_ALGORITHM, keyDerivationInfo.hmac),
+            saltHex = keyDerivationInfo.salt.toString("hex"),
+            pbkdf2Rounds = keyDerivationInfo.rounds;
+        // Perform encryption
+        var encryptedContent = encryptTool.update(text, "utf8", "base64");
+        encryptedContent += encryptTool.final("base64");
+        // Generate hmac
+        hmacTool.update(encryptedContent);
+        hmacTool.update(ivHex);
+        hmacTool.update(saltHex);
+        var hmacHex = hmacTool.digest("hex");
+        // Return packed content
+        return packing.packEncryptedContent(
+            encryptedContent,
+            ivHex,
+            saltHex,
+            hmacHex,
+            pbkdf2Rounds
+        );
+    },
 
-        encrypt: function(text, keyDerivationInfo) {
-            var iv = generation.generateIV(),
-                ivHex = iv.toString("hex");
-            var encryptTool = Crypto.createCipheriv(config.ENC_ALGORITHM, keyDerivationInfo.key, iv),
-                hmacTool = Crypto.createHmac(config.HMAC_ALGORITHM, keyDerivationInfo.hmac),
-                saltHex = keyDerivationInfo.salt.toString("hex"),
-                pbkdf2Rounds = keyDerivationInfo.rounds;
-            // Perform encryption
-            var encryptedContent = encryptTool.update(text, "utf8", "base64");
-            encryptedContent += encryptTool.final("base64");
-            // Generate hmac
-            hmacTool.update(encryptedContent);
-            hmacTool.update(ivHex);
-            hmacTool.update(saltHex);
-            var hmacHex = hmacTool.digest("hex");
-            // Return packed content
-            return packing.packEncryptedContent(
-                encryptedContent,
-                ivHex,
-                saltHex,
-                hmacHex,
-                pbkdf2Rounds
-            );
-        },
+    /**
+     * Encrypt some text using a file (filename or buffer)
+     * @param {string} text The text to encrypt
+     * @param {string|Buffer} filenameOrBuffer The filename or a buffer
+     * @param {Function=} callback An optional callback
+     * @returns {Promise.<string>} A promise that resolves with the encrypted text
+     */
+    encryptWithKeyFile: function(text, filenameOrBuffer, callback) {
+        debug("encrypt using keyfile");
+        return derivation.deriveFromFile(filenameOrBuffer)
+            .then((keyDerivationInfo) => lib.encrypt(text, keyDerivationInfo));
+    },
 
-        /**
-         * Encrypt some text using a file (filename or buffer)
-         * @param {string} text The text to encrypt
-         * @param {string|Buffer} filenameOrBuffer The filename or a buffer
-         * @param {Function=} callback An optional callback
-         * @returns {Promise.<string>} A promise that resolves with the encrypted text
-         */
-        encryptWithKeyFile: function(text, filenameOrBuffer, callback) {
-            return derivation.deriveFromFile(filenameOrBuffer)
-                .then((keyDerivationInfo) => lib.encrypt(text, keyDerivationInfo));
-        },
+    encryptWithPassword: function(text, password) {
+        debug("encrypt using password");
+        return derivation.deriveFromPassword(password)
+            .then((keyDerivationInfo) => lib.encrypt(text, keyDerivationInfo));
+    },
 
-        encryptWithPassword: function(text, password) {
-            return derivation.deriveFromPassword(password)
-                .then((keyDerivationInfo) => lib.encrypt(text, keyDerivationInfo));
-        },
-
-        decrypt: function(encryptedComponents, keyDerivationInfo) {
-            // Extract the components
-            var encryptedContent = encryptedComponents.content,
-                iv = new Buffer(encryptedComponents.iv, "hex"),
-                salt = encryptedComponents.salt,
-                hmacData = encryptedComponents.hmac;
-            // Get HMAC tool
-            var hmacTool = Crypto.createHmac(config.HMAC_ALGORITHM, keyDerivationInfo.hmac);
-            // Generate the HMAC
-            hmacTool.update(encryptedContent);
-            hmacTool.update(encryptedComponents.iv);
-            hmacTool.update(salt);
-            var newHmaxHex = hmacTool.digest("hex");
-            // Check hmac for tampering
-            if (security.constantTimeCompare(hmacData, newHmaxHex) !== true) {
-                throw new Error("Encrypted content has been tampered with");
-            }
-            // Decrypt
-            var decryptTool = Crypto.createDecipheriv(config.ENC_ALGORITHM, keyDerivationInfo.key, iv),
-                decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
-            return decryptedText + decryptTool.final("utf8");
-        },
-
-        decryptWithKeyFile: function(text, file, callback) {
-            var encryptedComponents = packing.unpackEncryptedContent(text);
-            return derivation.deriveFromFile(file, encryptedComponents.salt, encryptedComponents.rounds)
-                .then((keyDerivationInfo) => lib.decrypt(encryptedComponents, keyDerivationInfo));
-
-        },
-
-        decryptWithPassword: function(text, password) {
-            var encryptedComponents = packing.unpackEncryptedContent(text)
-            return derivation.deriveFromPassword(password, encryptedComponents.salt, encryptedComponents.rounds)
-                .then((keyDerivationInfo) => lib.decrypt(encryptedComponents, keyDerivationInfo));
+    decrypt: function(encryptedComponents, keyDerivationInfo) {
+        debug("decrypt content");
+        // Extract the components
+        var encryptedContent = encryptedComponents.content,
+            iv = new Buffer(encryptedComponents.iv, "hex"),
+            salt = encryptedComponents.salt,
+            hmacData = encryptedComponents.hmac;
+        // Get HMAC tool
+        var hmacTool = Crypto.createHmac(config.HMAC_ALGORITHM, keyDerivationInfo.hmac);
+        // Generate the HMAC
+        hmacTool.update(encryptedContent);
+        hmacTool.update(encryptedComponents.iv);
+        hmacTool.update(salt);
+        var newHmaxHex = hmacTool.digest("hex");
+        // Check hmac for tampering
+        if (security.constantTimeCompare(hmacData, newHmaxHex) !== true) {
+            throw new Error("Encrypted content has been tampered with");
         }
+        // Decrypt
+        var decryptTool = Crypto.createDecipheriv(config.ENC_ALGORITHM, keyDerivationInfo.key, iv),
+            decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
+        return decryptedText + decryptTool.final("utf8");
+    },
 
-    };
+    decryptWithKeyFile: function(text, file, callback) {
+        debug("decrypt using keyfile");
+        var encryptedComponents = packing.unpackEncryptedContent(text);
+        return derivation.deriveFromFile(file, encryptedComponents.salt, encryptedComponents.rounds)
+            .then((keyDerivationInfo) => lib.decrypt(encryptedComponents, keyDerivationInfo));
 
-})(module);
+    },
+
+    decryptWithPassword: function(text, password) {
+        debug("decrypt using password");
+        var encryptedComponents = packing.unpackEncryptedContent(text)
+        return derivation.deriveFromPassword(password, encryptedComponents.salt, encryptedComponents.rounds)
+            .then((keyDerivationInfo) => lib.decrypt(encryptedComponents, keyDerivationInfo));
+    }
+
+};
--- source/derive.js
+++ source/derive.js
@@ -1,82 +1,81 @@
-(function(module) {
+"use strict";
 
-    "use strict";
+var config = require("./config.js"),
+    generators = require("./generators.js"),
+    components = require("./components.js"),
+    debug = require("debug");
 
-    var config = require("./config.js"),
-        generators = require("./generators.js"),
-        components = require("./components.js");
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    function getRandomInRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+function sanitiseRounds(rounds) {
+    return rounds || getRandomInRange(
+        config.DERIVED_KEY_ITERATIONS_MIN,
+        config.DERIVED_KEY_ITERATIONS_MAX
+    );
+}
+
+function sanitiseSalt(salt) {
+    return salt || generators.generateSalt(config.SALT_LENGTH);
+}
+
+var lib = module.exports = {
+
+    /**
+     * Derive a key from a file, using its filename or contents
+     * @param {string|Buffer} filenameOrBuffer The filename or contents of the file
+     * @param {string=} salt The salt
+     * @param {number=} rounds The number of rounds
+     */
+    deriveFromFile: function(filenameOrBuffer, salt, rounds) {
+        debug("derive key from file");
+        return generators.generateFileHash(filenameOrBuffer)
+            .then(function(hash) {
+                return lib.deriveFromPassword(hash, salt, rounds);
+            });
+    },
+
+    /**
+     * Derived key info
+     * @typedef DerivedKeyInfo
+     * @property {string} salt - The salt used
+     * @property {Buffer} key - The derived key
+     * @property {Buffer} hmac - The HMAC
+     * @property {number} rounds - The number of rounds used
+     */
+
+    /**
+     * Derive a key from a password
+     * @param {string} password The password to derive from
+     * @param {string=} salt The salt (Optional)
+     * @param {number=} rounds The number of iterations
+     * @returns {Promise.<DerivedKeyInfo>} A promise that resolves with an object (DerivedKeyInfo)
+     */
+    deriveFromPassword: function(password, salt, rounds) {
+        debug("derive key from password");
+        rounds = sanitiseRounds(rounds);
+        salt = sanitiseSalt(salt);
+        let bits = (config.PASSWORD_KEY_SIZE + config.HMAC_KEY_SIZE)  * 8;
+        return components.getPBKDF2()(
+                password,
+                salt,
+                rounds,
+                bits,
+                config.DERIVED_KEY_ALGORITHM
+            )
+            .then((derivedKeyData) => derivedKeyData.toString("hex"))
+            .then(function(derivedKeyHex) {
+                let dkhLength = derivedKeyHex.length,
+                    keyBuffer = new Buffer(derivedKeyHex.substr(0, dkhLength / 2), "hex"),
+                    hmacBuffer = new Buffer(derivedKeyHex.substr(dkhLength / 2, dkhLength / 2), "hex");
+                return {
+                    salt: salt,
+                    key: keyBuffer,
+                    hmac: hmacBuffer,
+                    rounds: rounds
+                };
+            });
     }
 
-    function sanitiseRounds(rounds) {
-        return rounds || getRandomInRange(
-            config.DERIVED_KEY_ITERATIONS_MIN,
-            config.DERIVED_KEY_ITERATIONS_MAX
-        );
-    }
-
-    function sanitiseSalt(salt) {
-        return salt || generators.generateSalt(config.SALT_LENGTH);
-    }
-
-    var lib = module.exports = {
-
-        /**
-         * Derive a key from a file, using its filename or contents
-         * @param {string|Buffer} filenameOrBuffer The filename or contents of the file
-         * @param {string=} salt The salt
-         * @param {number=} rounds The number of rounds
-         */
-        deriveFromFile: function(filenameOrBuffer, salt, rounds) {
-            return generators.generateFileHash(filenameOrBuffer)
-                .then(function(hash) {
-                    return lib.deriveFromPassword(hash, salt, rounds);
-                });
-        },
-
-        /**
-         * Derived key info
-         * @typedef DerivedKeyInfo
-         * @property {string} salt - The salt used
-         * @property {Buffer} key - The derived key
-         * @property {Buffer} hmac - The HMAC
-         * @property {number} rounds - The number of rounds used
-         */
-
-        /**
-         * Derive a key from a password
-         * @param {string} password The password to derive from
-         * @param {string=} salt The salt (Optional)
-         * @param {number=} rounds The number of iterations
-         * @returns {Promise.<DerivedKeyInfo>} A promise that resolves with an object (DerivedKeyInfo)
-         */
-        deriveFromPassword: function(password, salt, rounds) {
-            rounds = sanitiseRounds(rounds);
-            salt = sanitiseSalt(salt);
-            let bits = (config.PASSWORD_KEY_SIZE + config.HMAC_KEY_SIZE)  * 8;
-            return components.getPBKDF2()(
-                    password,
-                    salt,
-                    rounds,
-                    bits,
-                    config.DERIVED_KEY_ALGORITHM
-                )
-                .then((derivedKeyData) => derivedKeyData.toString("hex"))
-                .then(function(derivedKeyHex) {
-                    let dkhLength = derivedKeyHex.length,
-                        keyBuffer = new Buffer(derivedKeyHex.substr(0, dkhLength / 2), "hex"),
-                        hmacBuffer = new Buffer(derivedKeyHex.substr(dkhLength / 2, dkhLength / 2), "hex");
-                    return {
-                        salt: salt,
-                        key: keyBuffer,
-                        hmac: hmacBuffer,
-                        rounds: rounds
-                    };
-                });
-        }
-
-    };
-
-})(module);
+};
