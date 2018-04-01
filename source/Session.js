a400eba1f067081c8f50deb3e7fc93db3f446ae7
Perry Mitchell <perry@perrymitchell.net>
2018-04-01 20:41:31 +0300

    Use global options for Session

--- source/Session.js
+++ source/Session.js
@@ -1,6 +1,13 @@
 const Configuration = require("./Configuration.js");
+const { getConfiguration } = require("./global.js");
 
 class Session extends Configuration {
+    constructor() {
+        super();
+        // Get options from global
+        this._options = Object.assign({}, getConfiguration().options);
+    }
+
     decrypt(text, password) {
 
     }
