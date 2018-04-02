5227bf9a04bb9c7ba21a5cfd239be4848eb74306
Perry Mitchell <perry@perrymitchell.net>
2018-04-02 12:13:00 +0300

    Set module docs

--- source/index.js
+++ source/index.js
@@ -1,14 +1,25 @@
 const { getConfiguration } = require("./global.js");
 const Session = require("./Session.js");
 
+/**
+ * Configure global values
+ * @returns {Configuration} The global configuration instance
+ */
 function configure() {
     return getConfiguration();
 }
 
+/**
+ * Start new encryption/decryption session
+ * @returns {Session} New crypto session
+ */
 function createSession() {
     return new Session();
 }
 
+/**
+ * @module iocane
+ */
 module.exports = {
     configure,
     createSession
