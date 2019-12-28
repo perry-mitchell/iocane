da283c7247ae410406d7b66e2d39d0e10df88377
Perry Mitchell <perry@perrymitchell.net>
2019-12-29 00:24:09 +0200

    Add web build

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.resolve(__dirname, "./source/index.web.ts"),
+
+    module: {
+        rules: [
+            {
+                test: /\.[jt]s$/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    node: {
+        buffer: "empty",
+        crypto: "empty"
+    },
+
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "./web")
+    },
+
+    resolve: {
+        extensions: [".ts", ".js", ".json"]
+    }
+};
