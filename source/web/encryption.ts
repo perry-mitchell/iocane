8e3fe7a37bf221c8b58a80c4f6c2eb29905b7195
Perry Mitchell <perry@perrymitchell.net>
2019-12-28 22:03:44 +0200

    Add web salt+IV generation

--- source/web/encryption.ts
+++ source/web/encryption.ts
@@ -1,4 +1,4 @@
-import * as crypto from "crypto";
+// import * as crypto from "crypto";
 import { constantTimeCompare } from "../base/timing";
 import { DerivedKeyInfo, EncryptedComponents, EncryptionType } from "../base/constructs";
 
@@ -6,62 +6,62 @@ const ENC_ALGORITHM_CBC = "aes-256-cbc";
 const ENC_ALGORITHM_GCM = "aes-256-gcm";
 const HMAC_ALGORITHM = "sha256";
 
-/**
- * Decrypt text using AES-CBC
- * @param encryptedComponents Encrypted components
- * @param keyDerivationInfo Key derivation information
- * @returns A promise that resolves with the decrypted string
- */
-export async function decryptCBC(
-    encryptedComponents: EncryptedComponents,
-    keyDerivationInfo: DerivedKeyInfo
-): Promise<string> {
-    // Extract the components
-    const encryptedContent = encryptedComponents.content;
-    const iv = new Buffer(encryptedComponents.iv, "hex");
-    const salt = encryptedComponents.salt;
-    const hmacData = encryptedComponents.auth;
-    // Get HMAC tool
-    const hmacTool = crypto.createHmac(HMAC_ALGORITHM, keyDerivationInfo.hmac);
-    // Generate the HMAC
-    hmacTool.update(encryptedContent);
-    hmacTool.update(encryptedComponents.iv);
-    hmacTool.update(salt);
-    const newHmaxHex = hmacTool.digest("hex");
-    // Check hmac for tampering
-    if (constantTimeCompare(hmacData, newHmaxHex) !== true) {
-        throw new Error("Authentication failed while decrypting content");
-    }
-    // Decrypt
-    const decryptTool = crypto.createDecipheriv(ENC_ALGORITHM_CBC, keyDerivationInfo.key, iv);
-    const decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
-    return `${decryptedText}${decryptTool.final("utf8")}`;
-}
+// /**
+//  * Decrypt text using AES-CBC
+//  * @param encryptedComponents Encrypted components
+//  * @param keyDerivationInfo Key derivation information
+//  * @returns A promise that resolves with the decrypted string
+//  */
+// export async function decryptCBC(
+//     encryptedComponents: EncryptedComponents,
+//     keyDerivationInfo: DerivedKeyInfo
+// ): Promise<string> {
+//     // Extract the components
+//     const encryptedContent = encryptedComponents.content;
+//     const iv = new Buffer(encryptedComponents.iv, "hex");
+//     const salt = encryptedComponents.salt;
+//     const hmacData = encryptedComponents.auth;
+//     // Get HMAC tool
+//     const hmacTool = crypto.createHmac(HMAC_ALGORITHM, keyDerivationInfo.hmac);
+//     // Generate the HMAC
+//     hmacTool.update(encryptedContent);
+//     hmacTool.update(encryptedComponents.iv);
+//     hmacTool.update(salt);
+//     const newHmaxHex = hmacTool.digest("hex");
+//     // Check hmac for tampering
+//     if (constantTimeCompare(hmacData, newHmaxHex) !== true) {
+//         throw new Error("Authentication failed while decrypting content");
+//     }
+//     // Decrypt
+//     const decryptTool = crypto.createDecipheriv(ENC_ALGORITHM_CBC, keyDerivationInfo.key, iv);
+//     const decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
+//     return `${decryptedText}${decryptTool.final("utf8")}`;
+// }
 
-/**
- * Decrypt text using AES-GCM
- * @param encryptedComponents Encrypted components
- * @param keyDerivationInfo Key derivation information
- * @returns A promise that resolves with the decrypted string
- */
-export async function decryptGCM(
-    encryptedComponents: EncryptedComponents,
-    keyDerivationInfo: DerivedKeyInfo
-): Promise<string> {
-    // Extract the components
-    const encryptedContent = encryptedComponents.content;
-    const iv = new Buffer(encryptedComponents.iv, "hex");
-    const { auth: tagHex } = encryptedComponents;
-    // Prepare tool
-    const decryptTool = crypto.createDecipheriv(ENC_ALGORITHM_GCM, keyDerivationInfo.key, iv);
-    // Add additional auth data
-    decryptTool.setAAD(new Buffer(`${encryptedComponents.iv}${keyDerivationInfo.salt}`, "utf8"));
-    // Set auth tag
-    decryptTool.setAuthTag(new Buffer(tagHex, "hex"));
-    // Perform decryption
-    const decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
-    return `${decryptedText}${decryptTool.final("utf8")}`;
-}
+// /**
+//  * Decrypt text using AES-GCM
+//  * @param encryptedComponents Encrypted components
+//  * @param keyDerivationInfo Key derivation information
+//  * @returns A promise that resolves with the decrypted string
+//  */
+// export async function decryptGCM(
+//     encryptedComponents: EncryptedComponents,
+//     keyDerivationInfo: DerivedKeyInfo
+// ): Promise<string> {
+//     // Extract the components
+//     const encryptedContent = encryptedComponents.content;
+//     const iv = new Buffer(encryptedComponents.iv, "hex");
+//     const { auth: tagHex } = encryptedComponents;
+//     // Prepare tool
+//     const decryptTool = crypto.createDecipheriv(ENC_ALGORITHM_GCM, keyDerivationInfo.key, iv);
+//     // Add additional auth data
+//     decryptTool.setAAD(new Buffer(`${encryptedComponents.iv}${keyDerivationInfo.salt}`, "utf8"));
+//     // Set auth tag
+//     decryptTool.setAuthTag(new Buffer(tagHex, "hex"));
+//     // Perform decryption
+//     const decryptedText = decryptTool.update(encryptedContent, "base64", "utf8");
+//     return `${decryptedText}${decryptTool.final("utf8")}`;
+// }
 
 /**
  * Encrypt text using AES-CBC
@@ -98,45 +98,47 @@ export async function encryptCBC(
     };
 }
 
-/**
- * Encrypt text using AES-GCM
- * @param text The text to encrypt
- * @param keyDerivationInfo Key derivation information
- * @param iv A buffer containing the IV
- * @returns A promise that resolves with encrypted components
- */
-export async function encryptGCM(
-    text: string,
-    keyDerivationInfo: DerivedKeyInfo,
-    iv: Buffer
-): Promise<EncryptedComponents> {
-    const ivHex = iv.toString("hex");
-    const { rounds } = keyDerivationInfo;
-    const encryptTool = crypto.createCipheriv(ENC_ALGORITHM_GCM, keyDerivationInfo.key, iv);
-    // Add additional auth data
-    encryptTool.setAAD(new Buffer(`${ivHex}${keyDerivationInfo.salt}`, "utf8"));
-    // Perform encryption
-    let encryptedContent = encryptTool.update(text, "utf8", "base64");
-    encryptedContent += encryptTool.final("base64");
-    // Handle authentication
-    const tag = encryptTool.getAuthTag();
-    // Output encrypted components
-    return {
-        method: EncryptionType.GCM,
-        iv: ivHex,
-        salt: keyDerivationInfo.salt,
-        rounds,
-        content: encryptedContent,
-        auth: tag.toString("hex")
-    };
-}
+// /**
+//  * Encrypt text using AES-GCM
+//  * @param text The text to encrypt
+//  * @param keyDerivationInfo Key derivation information
+//  * @param iv A buffer containing the IV
+//  * @returns A promise that resolves with encrypted components
+//  */
+// export async function encryptGCM(
+//     text: string,
+//     keyDerivationInfo: DerivedKeyInfo,
+//     iv: Buffer
+// ): Promise<EncryptedComponents> {
+//     const ivHex = iv.toString("hex");
+//     const { rounds } = keyDerivationInfo;
+//     const encryptTool = crypto.createCipheriv(ENC_ALGORITHM_GCM, keyDerivationInfo.key, iv);
+//     // Add additional auth data
+//     encryptTool.setAAD(new Buffer(`${ivHex}${keyDerivationInfo.salt}`, "utf8"));
+//     // Perform encryption
+//     let encryptedContent = encryptTool.update(text, "utf8", "base64");
+//     encryptedContent += encryptTool.final("base64");
+//     // Handle authentication
+//     const tag = encryptTool.getAuthTag();
+//     // Output encrypted components
+//     return {
+//         method: EncryptionType.GCM,
+//         iv: ivHex,
+//         salt: keyDerivationInfo.salt,
+//         rounds,
+//         content: encryptedContent,
+//         auth: tag.toString("hex")
+//     };
+// }
 
 /**
  * IV generator
  * @returns A newly generated IV
  */
-export async function generateIV(): Promise<Buffer> {
-    return new Buffer(crypto.randomBytes(16));
+export async function generateIV(): Promise<ArrayBuffer> {
+    const randArr = new Uint8Array(16);
+    getCrypto().getRandomValues(randArr);
+    return randArr.buffer;
 }
 
 /**
@@ -149,12 +151,23 @@ export async function generateSalt(length: number): Promise<string> {
     if (length <= 0) {
         throw new Error(`Failed generating salt: Invalid length supplied: ${length}`);
     }
+    const crypto = getCrypto();
     let output = "";
     while (output.length < length) {
-        output += crypto.randomBytes(3).toString("base64");
+        const randArr = new Uint8Array(3);
+        crypto.getRandomValues(randArr);
+        const rawOutput = randArr.reduce((progress, byte) => {
+            return progress + String.fromCharCode(byte);
+        }, "");
+        output += btoa(rawOutput);
         if (output.length > length) {
             output = output.substr(0, length);
         }
     }
     return output;
+}
+
+function getCrypto(): Crypto {
+    // @ts-ignore // msCrypto not recognised
+    return window.crypto || window.msCrypto;
 }
